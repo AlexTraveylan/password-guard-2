8305c2dd7483869b2e55992e1e3898591bbef940
AlexTraveylan <monsieurdemares@gmail.com>
2023-08-09 12:23:07 +0200

    modification mot de passe et switch light/dark mode

--- src/components/new-password.tsx
+++ src/components/new-password.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { MinusCircleIcon, PlusCircleIcon } from "../../node_modules/lucide-react"
 import { useState } from "react"
+import { MinusCircleIcon, PlusCircleIcon } from "../../node_modules/lucide-react"
 import { AddPasswordForm } from "./forms/add-password-form"
 
 export function NewPassword({ recupPasswords }: { recupPasswords: () => Promise<void> }) {
@@ -10,7 +10,7 @@ export function NewPassword({ recupPasswords }: { recupPasswords: () => Promise<
     <div className="flex flex-col items-center">
       <h1>Ajouter un mot de passe :</h1>
       {isShow ? <MinusCircleIcon size={30} onClick={() => setIsShow(!isShow)} /> : <PlusCircleIcon size={30} onClick={() => setIsShow(!isShow)} />}
-      <AddPasswordForm isShow={isShow} setIsShow={setIsShow} recupPasswords={recupPasswords} />
+      <AddPasswordForm setIsShow={setIsShow} recupPasswords={recupPasswords} />
     </div>
   )
 }
