a6ced31d2ce10b50c7d01a5aa0339e9cb72eedda
AlexTraveylan <monsieurdemares@gmail.com>
2023-08-09 21:56:07 +0200

    fix default switch theme

--- src/components/switch-theme.tsx
+++ src/components/switch-theme.tsx
@@ -23,7 +23,7 @@ export function SwitchToggle() {
   return (
     <>
       <div className="flex items-center gap-3">
-        <Switch id="airplane-mode" onCheckedChange={toogleActualTheme} />
+        <Switch id="airplane-mode" onCheckedChange={toogleActualTheme} checked={actualTheme === "dark"} />
         <div className="inline-flex items-center justify-center rounded-md text-sm font-medium">
           <Sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
           <Moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
